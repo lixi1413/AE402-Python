0f43610c6343a3d042e11745e0df28cb1953aaea
lixi1413 <78331893+lixi1413@users.noreply.github.com>
2021-02-03 16:05:27 +0800

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,20 @@
+n=int(input("請使用者輸入全班幾人"))
+score=[]
+highest=0
+lowest=100
+for i in range(n):
+   mathscore=int(input("數學成績?"))
+   if highest<mathscore:
+       highest=mathscore
+   if lowest>mathscore:
+       lowest=mathscore
+   score.append(mathscore)
+   print(score)
+   print("最高",highest)
+   print("最低",lowest)
+def add3(x,y):
+    return add3
+    x=(highest(x))
+    y=(lowest(y))
+print(add3)
+print("結束")
