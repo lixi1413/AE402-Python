7ebe7975ec8b663fd47ddea0cb8aeb16dad13539
lixi1413 <78331893+lixi1413@users.noreply.github.com>
2021-02-03 14:12:53 +0800

    Add files via upload

--- untitled2.py
+++ untitled2.py
@@ -1,8 +1,24 @@
-score=input("請使用者輸入成績")
-score=int(score)
-if (score>=60):
-    print("及格")
-else:
-        print("不及格")
-
-
+n=int(input("請使用者輸入全班幾人"))
+score=[]
+highest=0
+lowest=100
+highestname=""
+lowestname=""
+summath=0
+for i in range(0,2*n):
+    mathscore=input("輸入")
+    score.append(mathscore)
+    print(score)
+    if i%2==1:
+        score[i]=int(score[i])
+        summath=summath+score[i]
+        score.append(mathscore)
+        if highest<score[i]:
+            higest=score[i]
+        if lowest>score[i]:
+            lowest=score[i]
+print(score)
+print("最高",highest,highestname)
+print("最低",lowest,lowestname)
+print("班上平均",summath/n)
+print("結束")
